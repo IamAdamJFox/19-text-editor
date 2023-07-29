c33eac61ae19f81c3d160bda8fedd7d96a705e98
adamjfox <adamjeffersonfox@gmail.com>
2023-07-28 23:19:25 -0700

    done just need to add readme

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,33 +12,55 @@ const initdb = async () =>
     },
   });
 
-  export const putDb = async (content) => {
-    console.log("Put to the database");
+// Function to save content to the "jate" IndexedDB
+export const putDb = async (content) => {
+  console.log("Putting data to the database");
 
-  const jateDb = await openDB("jate", 1);//in this line we will open the "jate" IndexedDb, await will stop the code untill the result 
-  //returns. the result will then be stored in "jateDb" 
-  const tx = jateDb.transaction("jate", "readwrite");//in this line we will start a new read/write transaction on the "jate" object.
-  //the transaction will the be stores in the "tx" constant
-  const store = tx.objectStore("jate");//in this line we access the stored "object" and assign it to const store
-  const request = store.put({ id: 1, value: content });//this line will put the value of id:1 into the saved object 
+  // Open the "jate" IndexedDB and wait until it's opened.
+  const jateDb = await openDB("jate", 1);
+
+  // Start a new read/write transaction on the "jate" object store.
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  // Access the "jate" object store to store data.
+  const store = tx.objectStore("jate");
+
+  // Put the given content with an ID of 1 into the "jate" object store.
+  const request = store.put({ id: 1, value: content });
+
+  // Log the request object, which represents the put operation.
   console.log(request);
 
+  // Await the result of the put operation.
   const result = await request;
-  console.log("data saved to the database", result);
+
+  // Log a success message and return the saved value.
+  console.log("Data saved to the database", result);
   return result?.value;
-  }
+};
 
-
-// TODO: Add logic for a method that gets all the content from the database
-//the following function will allow the opining of the jate database and retrieve all associated record 
+// Function to get all content from the "jate" IndexedDB
 export const getDb = async () => {
-  console.log("GET from the database");
+  console.log("Getting data from the database");
 
-  const jateDb = await openDB("jate", 1);//this line will allow the opening of "jate" the await keyword means the code will
-  //pause untill the database has been opened. the number 1 refers to the version of the object "jate"
-  const tx = jateDb.transaction("jate", "readonly");//this line will open a transaction of "jate" in read only
+  // Open the "jate" IndexedDB and wait until it's opened.
+  const jateDb = await openDB("jate", 1);
+
+  // Start a new read-only transaction on the "jate" object store.
+  const tx = jateDb.transaction("jate", "readonly");
+
+  // Access the "jate" object store to retrieve data.
   const store = tx.objectStore("jate");
+
+  // Get all records from the "jate" object store.
   const request = store.getAll();
-}
+
+  // Await the result of the getAll() operation to retrieve data.
+  const result = await request;
+
+  // Return the retrieved data.
+  return result;
+};
+
 
 initdb();
